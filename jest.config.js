3527f52880f591b913e54d3bd46dd957a9dc820a
qidu <teric@outlook.com>
2026-03-08 00:41:36 +0800

    add config

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@providers/(.*)$': '<rootDir>/src/providers/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1'
+  }
+};
